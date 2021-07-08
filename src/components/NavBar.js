2c5a9ed556855b72ff7bbf51f00de7ede2b60b6a
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-07-08 15:20:42 -0400

    home page added

---
+++ src/components/NavBar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+
+const NavBar = () => {
+  return (
+    <header className="bg-gray-500">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-left py-6 px-3 mr-4 text-gray-100 hover:text-gray-600 text-4xl font-bold tracking-widest"
+          >
+            Wichmann
+          </NavLink>
+          <NavLink
+            to="about"
+            activeClassName="text-white"
+            className="inflex-flex items-center py-3 px-3 mr-6 text-gray-400 hover:text-gray-600 text-4xl font-bold tracking-widest"
+          >
+            About Me
+          </NavLink>
+          <NavLink
+            to="project"
+            activeClassName="text-white"
+            className="inflex-flex items-center py-3 px-3 mr-4 text-gray-400 hover:text-gray-600 text-4xl font-bold tracking-widest"
+          >
+            Portfolio
+          </NavLink>
+          <NavLink
+            to="post"
+            activeClassName="text-white"
+            className="inflex-flex items-center py-3 px-3 mr-4 text-gray-400 hover:text-gray-600 text-4xl font-bold tracking-widest"
+          >
+            Contact
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-3 px-3 my-6 ">
+          <SocialIcon
+            url="https://github.com/NathanWichmann"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/nathan-wichmann-731a7219a/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://stackoverflow.com/users/16409018/nathan-wichmann"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
---
+++ src/components/Post.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client.js";
+
+const Post = () => {
+  const [postData, setPost] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type === "post]{
+            title,
+            slug,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                },
+                alt
+            }
+        }`
+      )
+      .then((data) => setPost(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main>
+      <h1></h1>
+      <h2></h2>
+      <div>
+        <article>
+          {/* <Link> */}
+            <span>
+              <img />
+              <span>
+                <h3></h3>
+              </span>
+            </span>
+          {/* </Link> */}
+        </article>
+      </div>
+    </main>
+  );
+};
+
+export default Post;
